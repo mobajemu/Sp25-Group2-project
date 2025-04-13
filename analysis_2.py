b63050dc6b1db6b57b9625a7ea7c1f5b18753f54
Michael Obajemu <33107179+mobajemu@users.noreply.github.com>
2025-04-12 22:00:22 -0400

    initial

---
+++ analysis_2.py
@@ -0,0 +1,10 @@
+### 2. Issue Reopen Rate
+
+**Insight:** Which types of issues are most likely to be reopened?
+
+**Input:** None
+
+**Output:** Bar chart
+
+- X axis - Labels
+- Y axis - % of issues that get reopened
